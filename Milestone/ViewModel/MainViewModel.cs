f66b0068fe2a9fd4d63cb054346927afe314dcdd
Chr0n1x <michael@mquandt.com>
2011-12-26 22:09:23 +1100

    Added Login View and did basic styling for the main view and login view.

--- Milestone/ViewModel/MainViewModel.cs
+++ Milestone/ViewModel/MainViewModel.cs
@@ -21,14 +21,14 @@ namespace Milestone.ViewModel
         /// </summary>
         public MainViewModel()
         {
-            ////if (IsInDesignMode)
-            ////{
-            ////    // Code runs in Blend --> create design time data.
-            ////}
-            ////else
-            ////{
-            ////    // Code runs "for real"
-            ////}
+            if (IsInDesignMode)
+            {
+                // Code runs in Blend --> create design time data.
+            }
+            else
+            {
+                // Code runs "for real"
+            }
         }
     }
 }
