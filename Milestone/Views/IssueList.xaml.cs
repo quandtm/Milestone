c00824fe31361d464e86bedfc85269cfada80038
Chr0n1x <michael@mquandt.com>
2011-12-27 02:59:58 +1100

    Added IssueList.xaml, also added starter code for parsing the query string that it receives. Added interaction link between Repo list and Issue List.

---
+++ Milestone/Views/IssueList.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+
+namespace Milestone.Views
+{
+    public partial class IssueList : PhoneApplicationPage
+    {
+        public IssueList()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var contextIndex = int.Parse(NavigationContext.QueryString["context"]);
+            var repoName = NavigationContext.QueryString["repo"];
+
+            base.OnNavigatedTo(e);
+        }
+    }
+}
--- Milestone/Views/MainPage.xaml.cs
+++ Milestone/Views/MainPage.xaml.cs
@@ -2,14 +2,16 @@
 using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
-using Milestone.Model;
 using Milestone.ViewModel;
+using NGitHub.Models;
 
 namespace Milestone.Views
 {
     public partial class MainPage : PhoneApplicationPage
     {
         private MainViewModel _vm;
+        private bool _ignoreRepoSelection = false;
+        private int _contextIndex;
 
         // Constructor
         public MainPage()
@@ -44,7 +46,29 @@ namespace Milestone.Views
                 return;
 
             _vm.SelectedContext = _vm.Model.Contexts[contextSelector.SelectedIndex];
+            _contextIndex = contextSelector.SelectedIndex;
             _vm.Refresh();
+        }
+
+        private void SelectRepo(object sender, SelectionChangedEventArgs e)
+        {
+            if (_ignoreRepoSelection)
+                return;
+
+            var listbox = sender as ListBox;
+            if (listbox == null || listbox.SelectedItem == null)
+                return;
+
+            var repo = listbox.SelectedItem as Repository;
+            if (repo == null)
+                return;
+
+            // Reset the selection
+            _ignoreRepoSelection = true;
+            listbox.SelectedIndex = -1;
+            _ignoreRepoSelection = false;
+
+            NavigationService.Navigate(new Uri("/Views/IssueList.xaml?context=" + _contextIndex + "&repo=" + repo.Name.Replace(" ", "%20"), UriKind.Relative));
         }
     }
 }
