ac15b70eb5b53238e9ae309a57eb6c6c1def2945
aeoth <paul@theleagueofpaul.com>
2011-12-29 18:23:22 +1100

    init

---
+++ js/jsLoader.js
@@ -0,0 +1,6 @@
+function require(jspath) {
+    document.write('<script type="text/javascript" src="'+jspath+'"><\/script>');
+}
+
+require("https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js");
+require("js/plugins/easySlider1.7.min.js");
